44184ed8ff90eb41bf844803f6ad679724aefe73
Iaroslav Verstiuk <iaroslav.verstiuk@osf-global.com>
2015-12-10 13:47:57 +0200

    popup

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -1,7 +1,16 @@
 @include('layouts.components.htmlhead')
 <body>
+	<div class="hover"></div>
+		<div class="popup">
+			<div class="close">X</div>
+				<div class="popup-content">
+					<p class="data-content"></p>
+				</div>
+		</div>
+	<div id="main">
 	@include('layouts.components.header')
 		@yield('content')
 	@include('layouts.components.footer')
+	</div>
 </body>
 </html>
