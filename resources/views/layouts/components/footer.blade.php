456e5f0368820e126447a345cff03780bb33c4b0
Roman Sabadash <r.v.sabadash@gmail.com>
2015-12-10 12:42:14 +0200

    Revert "Footer"
    This reverts commit c0e827b0fc59d933e3edf2b6fecd8ca8b0efae93.

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,11 +1,3 @@
-	<footer>
-		<img class='footer-logo' src="css/img/logo.png">
-		<div class="social">
-			<img class='social-image' src="css/img/insta.png">
-			<img class='social-image' src="css/img/fb.png">
-			<img class='social-image' src="css/img/vk.png">
-		</div>
-		<div class="footer-darker">© 2015 Event Company TN. All rights reserved</div>
-	</footer>
+	<footer></footer>
 <script src="{{asset('js/jquery-2.1.4.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
